f87431bdc7ee07266276c8660662473e9c058465
kaiqiang cao <kevinswiftios@gmail.com>
2020-03-09 19:39:10 +0800

    commit ckq1

--- egg-demo/test/app/controller/home.test.js
+++ egg-demo/test/app/controller/home.test.js
@@ -4,6 +4,7 @@ const { app, assert } = require('egg-mock/bootstrap');
 
 describe('test/app/controller/home.test.js', () => {
   it('should assert', () => {
+    console.log(112233);
     const pkg = require('../../../package.json');
     assert(app.config.keys.startsWith(pkg.name));
 
